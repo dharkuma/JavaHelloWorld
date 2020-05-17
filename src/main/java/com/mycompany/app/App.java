c46e13264a6b69bc692932309e43f3961778941e
dharkuma <dharmesh.kr2010@example.com>
2020-05-18 02:01:08 +0530

    updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World from Dharmesh Kumar!";
+    private final String message = "Hello World from Dharmesh!";
 
     public App() {}
 
